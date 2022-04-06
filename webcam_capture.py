1fdce3e53ef585c77228377816bd97dd14bc6085
rounaksalim95 <rounaksalim@gmail.com>
2022-04-06 20:36:47 +0530

    Add basic code for capturing images from webcam; also add requirements.txt

---
+++ webcam_capture.py
@@ -0,0 +1,19 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+# Read the image
+result, image = cam.read()
+
+if result:
+    cv2.imshow("test", image)
+
+    # Save image in local storage
+    # cv2.imwrite("test.png", image)
+
+    # Press key to destroy window
+    cv2.waitKey(0)
+    cv2.destroyWindow("test")
+
+else:
+    print("Error in capturing image")
