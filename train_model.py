81287e7fb022c3de06bbd2881ddd58bfbc6e38b5
rounaksalim95 <rounaksalim@gmail.com>
2022-05-24 15:23:57 -0700

    Update train_model.py to train model with new custom dataset

--- train_model.py
+++ train_model.py
@@ -10,6 +10,7 @@ import PIL
 import tensorflow as tf
 
 from sklearn.utils import shuffle
+from sklearn.metrics import classification_report
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
@@ -37,22 +38,18 @@ def main():
     test_images = []
 
     # Read train images
-    read_images("train/ok-sign", 1, 2401, train_images)
-    read_images("train/l-sign", 1, 2401, train_images)
-    read_images("train/victory-sign", 1, 2401, train_images)
-    read_images("train/fingers-crossed", 1, 2401, train_images)
-    read_images("train/thumbs-up", 1, 2401, train_images)
-
-    print(len(train_images))
+    read_images("train/0_ok_sign", 1, 2401, train_images)
+    read_images("train/1_l_sign", 1, 2401, train_images)
+    read_images("train/2_victory_sign", 1, 2401, train_images)
+    read_images("train/3_fingers_crossed", 1, 2401, train_images)
+    read_images("train/4_thumbs_up", 1, 2401, train_images)
 
     # Read test images
-    read_images("test/ok-sign", 2401, 3201, test_images)
-    read_images("test/l-sign", 2401, 3201, test_images)
-    read_images("test/victory-sign", 2401, 3201, test_images)
-    read_images("test/fingers-crossed", 2401, 3201, test_images)
-    read_images("test/thumbs-up", 2401, 3201, test_images)
-
-    print(len(test_images))
+    read_images("test/0_ok_sign", 2401, 3201, test_images)
+    read_images("test/1_l_sign", 2401, 3201, test_images)
+    read_images("test/2_victory_sign", 2401, 3201, test_images)
+    read_images("test/3_fingers_crossed", 2401, 3201, test_images)
+    read_images("test/4_thumbs_up", 2401, 3201, test_images)
 
     # Generate train labels
     train_labels = []
@@ -91,8 +88,6 @@ def main():
     for i in range(800):
         test_labels.append([0, 0, 0, 0, 1])
 
-    print(len(train_labels))
-    print(len(test_labels))
 
     # Convert to numpy arrays
     train_images = np.array(train_images)
@@ -107,7 +102,7 @@ def main():
     # Create model
     model = Sequential([
         layers.Rescaling(1./255, input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 1)),
-        layers.Conv2D(16, 3, padding="same" activation="relu"),
+        layers.Conv2D(16, 3, padding="same", activation="relu"),
         layers.MaxPool2D(padding="same"),
         layers.Conv2D(32, 3, padding="same", activation="relu"),
         layers.MaxPool2D(padding="same"),
@@ -126,6 +121,39 @@ def main():
 
     epochs = 10
     history = model.fit(x = train_images, y = train_labels, validation_data = (test_images, test_labels), epochs = epochs)
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+
+    # classification report
+    
+    pred = model.predict(test_images)
+
+    y_pred=np.argmax(pred, axis=1)
+    y_test=np.argmax(test_labels, axis=1)
+
+    print(classification_report(y_test, y_pred))
+
+    model.save("FinalModel/CustomDataGestureRecognitionModelBig.tfl")
 
 
 if __name__ == '__main__':
